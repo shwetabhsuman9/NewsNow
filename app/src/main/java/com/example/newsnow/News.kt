6df84bfcf9c87f61fafb4e1d19d5376fb741825f
Shwetabh suman <shwetabhsuman3@gmail.com>
2021-04-15 03:40:12 +0530

    NewsNow

---
+++ app/src/main/java/com/example/newsnow/News.kt
@@ -0,0 +1,10 @@
+package com.example.newsnow
+
+import androidx.appcompat.widget.DialogTitle
+
+data class News (
+    val title: String,
+    val author: String,
+    val url : String,
+    val imageUrl : String
+        )
